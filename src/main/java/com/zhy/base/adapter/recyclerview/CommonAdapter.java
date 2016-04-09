34ad33febf7506e92855b8f9becf2b2e7d73e9a4
hongyangAndroid <lmj623565791@gmail.com>
2016-04-10 00:58:33 +0800

    修改文件结构

--- src/main/java/com/zhy/base/adapter/recyclerview/CommonAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.zhy.base.adapter.recyclerview;
-
-/**
- * Created by zhy on 16/4/9.
- */
-public class CommonAdapter
-{
-}
--- src/main/java/com/zhy/base/adapter/recyclerview/OnItemClickListener.java
+++
@@ -1,5 +0,0 @@
-package com.zhy.base.adapter.recyclerview;
-
-public interface OnItemClickListener {
-        void onItemClick(RecyclerView parent, View view, int position, long id);
-    }
